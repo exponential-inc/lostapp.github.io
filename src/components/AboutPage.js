fc6a00bf2e4585e31ac8f73c252b705eda859560
zeroEighteen <koh_yi_lei_elliot@s2019.ssts.edu.sg>
2020-06-29 18:36:45 +0800

    Add Desktop About Page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,17 +7,27 @@ import thinking from './../images/thinking.svg';
 import wheelchair from './../images/wheelchair.svg';
 import iphone_x from './../images/iphone-x-flat-buttons.svg';
 export class AboutPage extends Component {
+	desktopStyles = ['3rem', 'flex', 'row', '30vw', '1rem', '-20vh'];
+
+	mobileStyles = ['2rem', 'block', 'column', '90vw', '2rem', '0'];
+	responsiveStyles = (idNum) => {
+		if (this.props.isDesktop === true) {
+			return this.desktopStyles[idNum];
+		} else {
+			return this.mobileStyles[idNum];
+		}
+	};
 	render() {
 		return (
 			<div>
 				<section id='about'>
-					<h2 id='dyk-header' data-aos='fade-up' data-aos-duration='750' data-aos-delay='1750'>
+					<h2 id='dyk-header' data-aos='fade-up' data-aos-duration='750' data-aos-delay='1750' style={{fontSize: this.responsiveStyles(0)}}>
 						Did you know?
 					</h2>
-					<div id='infocard-container'>
-						<InfoCard imgSrc={thinking} textContent='Dementia is a syndrome in which there is deterioration in memory, thinking, behaviour and the ability to perform everyday activities.' />
-						<InfoCard imgSrc={wheelchair} textContent='Dementia is one of the major causes of disability and dependency among older people' />
-						<InfoCard imgSrc={world_map} textContent='50 million people suffer from dementia worldwide' />
+					<div id='infocard-container' style={{display: this.responsiveStyles(1)}}>
+						<InfoCard imgSrc={thinking} textContent='Dementia is a syndrome in which there is deterioration in memory, thinking, behaviour and the ability to perform everyday activities.' isDesktop={this.props.isDesktop} />
+						<InfoCard imgSrc={wheelchair} textContent='Dementia is one of the major causes of disability and dependency among older people' isDesktop={this.props.isDesktop} />
+						<InfoCard imgSrc={world_map} textContent='50 million people suffer from dementia worldwide' isDesktop={this.props.isDesktop} />
 					</div>
 					<h2 id='dyk-header' data-aos='fade-up' data-aos-duration='750' data-aos-delay='250'>
 						that's where
@@ -28,14 +38,14 @@ export class AboutPage extends Component {
 					<p id='about-lost-subheader' data-aos='fade-up' data-aos-duration='950' data-aos-delay='250'>
 						lost helps you locate family members that need help with navigation, easily and effectively
 					</p>
-					<div className='features-container'>
+					<div className='features-container' style={{flexDirection: this.responsiveStyles(2), margin: this.responsiveStyles(4)}}>
 						<FeaturesText textContentHeader='View their location' textContentDesc={`Built with Google Map's API, our in-app map will clearly display the location of your loved one with accuracy of up to a 5m radius`} />
 						<FeaturesText textContentHeader='View their location' textContentDesc={`Built with Google Map's API, our in-app map will clearly display the location of your loved one with accuracy of up to a 5m radius`} />
-						<img src={iphone_x} alt='' id='iphone-x' data-aos='fade-up' data-aos-duration='750' data-aos-delay='250' />
+						<img src={iphone_x} alt='' id='iphone-x' data-aos='fade-up' data-aos-duration='750' data-aos-delay='250' style={{minWidth: this.responsiveStyles(3)}} />
 						<FeaturesText textContentHeader='View their location' textContentDesc={`Built with Google Map's API, our in-app map will clearly display the location of your loved one with accuracy of up to a 5m radius`} />
 						<FeaturesText textContentHeader='View their location' textContentDesc={`Built with Google Map's API, our in-app map will clearly display the location of your loved one with accuracy of up to a 5m radius`} />
 					</div>
-					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320' className='wave' id='wave2'>
+					<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320' className='wave' id='wave2' style={{marginTop: this.responsiveStyles(5)}}>
 						<path
 							fill='#f8f8f8'
 							fill-opacity='1'
--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
 
 export class InfoCard extends Component {
+	desktopStyles = ['30vw', '1rem'];
+
+	mobileStyles = ['90vw', '1rem 0'];
+	responsiveStyles = (idNum) => {
+		if (this.props.isDesktop === true) {
+			return this.desktopStyles[idNum];
+		} else {
+			return this.mobileStyles[idNum];
+		}
+	};
 	render() {
 		return (
-			<div id='card-container' data-aos='fade-right' data-aos-duration='1000' data-aos-delay='100'>
+			<div id='card-container' data-aos='fade-right' data-aos-duration='1000' data-aos-delay='100' style={{width: this.responsiveStyles(0), margin: this.responsiveStyles(1)}}>
 				<div className='card-part' id='left-side'>
 					<img className='infocard-img' src={this.props.imgSrc} alt='' />
 				</div>
